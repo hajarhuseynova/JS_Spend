0897fa05c3627c5bdd382001c2b4d75d9a531910
hajar298 <hajarih@code.edu.az>
2023-05-24 18:35:13 +0400

    add input part

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const price = document.querySelectorAll(".card>h4");
 var totalAll = 0;
 var countAddItemAll = 0;
 let totalMoneyAmount = 187000000000;
+const basket = [];
 
 //totaldiv
 let totalDiv = document.createElement("div");
@@ -19,21 +20,46 @@ let totalName = document.createElement("h3");
 totalName.textContent = "Total";
 let totalValue = document.createElement("h3");
 totalValue.textContent = "$0.00";
-// totalValue.textContent = totalCount - totalAll;
+
 totalDiv.append(totalName, totalValue);
 
 for (let i = 0; i < card.length; i++) {
-  buyButton[i].addEventListener("click", () => {
-    receipt.style.display = "block";
+  let value = 0;
+  countItem[i].addEventListener("change", () => {
+    buyButton[i].style.backgroundColor = "rgb(162, 155, 254)";
     const priceNum = parseFloat(
       price[i].textContent.replace("$", "").replaceAll(",", "")
     );
-    countAddItemAll = ++countItem[i].value;
-    sellButton[i].style.backgroundColor = "red";
-    const itemPrice = priceNum;
 
-    //check
+    let totalCount = parseFloat(
+      totalAmount.textContent.replace("$", "").replaceAll(",", "")
+    );
+    totalCount += value;
+    if (totalCount < countItem[i].value * priceNum) {
+      countItem[i].value = Math.trunc(totalCount / priceNum);
+
+      buyButton[i].style.backgroundColor = "rgb(220,220,220)";
+    }
+    if (countItem[i].value != 0) {
+      sellButton[i].style.backgroundColor = "red";
+    }
+    countAddItemAll = countItem[i].value;
+    totalCount -= countItem[i].value * priceNum;
+    totalAll = 0;
+    totalAll += countItem[i].value * priceNum;
+    value = countItem[i].value * priceNum;
+    totalAmount.textContent = "$" + totalCount;
+    totalValue.textContent = "$" + totalAll;
+    createReceipt();
+  });
+}
+
+for (let i = 0; i < card.length; i++) {
+  function createReceipt() {
     const existingItemDiv = document.getElementById(`item-${i}`);
+    const priceNum = parseFloat(
+      price[i].textContent.replace("$", "").replaceAll(",", "")
+    );
     if (existingItemDiv) {
       // Update existing item div
       const itemCount = existingItemDiv.querySelector(".item-count");
@@ -56,17 +82,34 @@ for (let i = 0; i < card.length; i++) {
       itemPriceElement.textContent = "$" + priceNum * countAddItemAll;
       itemPriceElement.classList.add("item-price");
 
-      // // line
-      // let line = document.createElement("div");
-      // line.style.width = "80%";
-      // line.style.margin = "auto";
-      // line.style.height = "1px";
-      // line.style.borderBottom = "1px dashed rgb(255, 255, 255)";
+      // line
+      let line = document.createElement("div");
+      line.style.width = "80%";
+      line.style.margin = "auto";
+      line.style.height = "1px";
+      line.style.borderBottom = "1px dashed rgb(255, 255, 255)";
       //append
       itemDiv.append(itemName, itemCount, itemPriceElement);
       items.appendChild(itemDiv);
-      items.append(totalDiv);
+      items.append(line, totalDiv);
     }
+  }
+  buyButton[i].addEventListener("click", () => {
+    receipt.style.display = "block";
+    const priceNum = parseFloat(
+      price[i].textContent.replace("$", "").replaceAll(",", "")
+    );
+    countAddItemAll = ++countItem[i].value;
+    sellButton[i].style.backgroundColor = "red";
+    const itemPrice = priceNum;
+    // if (totalCount < countItem[i].value * priceNum) {
+    //   countItem[i].value = Math.trunc(totalCount / priceNum);
+
+    //   buyButton[i].style.backgroundColor = "rgb(220,220,220)";
+    // }
+
+    //check
+    createReceipt();
     const totalCount = parseFloat(
       totalAmount.textContent.replace("$", "").replaceAll(",", "")
     );
@@ -77,6 +120,7 @@ for (let i = 0; i < card.length; i++) {
   });
 
   sellButton[i].addEventListener("click", () => {
+    buyButton[i].style.backgroundColor = "rgb(162, 155, 254)";
     if (countItem[i].value > 0) {
       let countRemoveItemAll = --countItem[i].value;
       const priceNum = parseFloat(
@@ -90,6 +134,7 @@ for (let i = 0; i < card.length; i++) {
           totalAmount.textContent.replace("$", "").replaceAll(",", "")
         ) +
           priceNum);
+
       //check
       const existingItemDiv = document.getElementById(`item-${i}`);
       if (existingItemDiv) {
@@ -98,7 +143,7 @@ for (let i = 0; i < card.length; i++) {
         const itemPriceElement = existingItemDiv.querySelector(".item-price");
         itemCount.textContent = countRemoveItemAll;
         itemPriceElement.textContent = itemPrice;
-        // Remove item div if count becomes 0
+        // Remove
         if (countRemoveItemAll === 0) {
           existingItemDiv.remove();
         }
@@ -111,3 +156,8 @@ for (let i = 0; i < card.length; i++) {
     totalValue.textContent = "$" + totalAll;
   });
 }
+
+// function calculateTotal() {
+//   receipt.innerHTML = "";
+//   basket.forEach();
+// }
